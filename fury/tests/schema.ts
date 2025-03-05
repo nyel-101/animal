d3d11141e39f16aff791e915ef955b914843c575
nyel-101 <dandevjr@gmail.com>
2025-03-05 20:52:36 +0000

    test(core): refactor auth tokens

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b058a47bd8fa4053b698b1841930e5e9 = () => {
-  return '2026-04-27T08:05:27Z';
+const node_2c670c5a7ee14cda9adb56ece60812bc = () => {
+  return '2025-03-05T20:52:36Z';
 };
