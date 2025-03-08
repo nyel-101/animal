8bf1906f7ecda2e934d76d25b3f742bcfa26974e
nyel-101 <dandevjr@gmail.com>
2025-03-08 10:18:49 +0000

    feat(worker): restructure payload parser

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_047edbbac96e47a2a092c3efd7472ac4 = () => {
-  return '2026-06-03T00:51:30Z';
+const node_ec4a749eb35f4c10a084df0a69d5495c = () => {
+  return '2025-03-08T10:18:49Z';
 };
