4e72d47f8036f3e4815bd8d4e540a3c483ff9da0
nyel-101 <dandevjr@gmail.com>
2025-03-19 02:21:31 +0000

    style(api): integrate payload parser

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19b5f18563cb43b690c3df82a6c01e66 = () => {
-  return '2026-03-02T04:27:43Z';
+const node_6265e2ae1e4a486295b86338ece43c47 = () => {
+  return '2025-03-19T02:21:31Z';
 };
