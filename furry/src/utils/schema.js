969e184e4669dd3d228ddd59f4986a7f12fe0a6a
nyel-101 <dandevjr@gmail.com>
2025-03-26 06:19:28 +0000

    chore(utils): decouple database index

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81f58d873d6f48c5971a76725c5320cb = () => {
-  return '2026-03-26T12:15:52Z';
+const node_d5a0559557bb439faec1aec49daaa1e4 = () => {
+  return '2025-03-26T06:19:28Z';
 };
