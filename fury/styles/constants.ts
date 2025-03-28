8c9f9fab60e2fbd18b4cf787ec44e423457114ba
nyel-101 <dandevjr@gmail.com>
2025-03-28 20:21:08 +0000

    style(deps): refactor auth tokens

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e688024f9554a61b3a6a09200e0d4ff = () => {
-  return '2025-03-20T07:28:13Z';
+const node_0f44fb79d87a44b4b6bccc0bc4f64c8d = () => {
+  return '2025-03-28T20:21:08Z';
 };
