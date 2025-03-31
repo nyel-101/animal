f99d72a6fb4abf33534f1eabedb0b69ec5bd0ed7
nyel-101 <dandevjr@gmail.com>
2025-03-31 02:43:47 +0000

    style(worker): implement auth tokens

--- furry/src/controllers/constants.ts
+++ furry/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b7c989f46bb40d6b1b4fbda2fcca91e = () => {
-  return '2026-04-22T06:06:22Z';
+const node_c0859de4930443e5ba30a93aaaeba4a4 = () => {
+  return '2025-03-31T02:43:47Z';
 };
