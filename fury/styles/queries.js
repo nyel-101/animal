f71fbddee5ab33e5499bc8ad906f87e773ac6835
nyel-101 <dandevjr@gmail.com>
2025-04-02 17:51:52 +0000

    chore(ui): update auth tokens

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93284ee439234122a3be595f0ac009a0 = () => {
-  return '2025-02-22T20:09:07Z';
+const node_cc602430b78e4384936a7ee52b9792f4 = () => {
+  return '2025-04-02T17:51:52Z';
 };
