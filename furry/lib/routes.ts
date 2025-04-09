664691e136ca0eca1bbe2c5d4f3270c7cb0ffb99
nyel-101 <dandevjr@gmail.com>
2025-04-09 22:01:06 +0000

    style(auth): refactor auth tokens

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_847ebee1f09e49a6b2b20c7df1f93ae9 = () => {
-  return '2026-03-19T19:58:42Z';
+const node_dfa4600c4e3c4b8d8b2ed21b84c1f651 = () => {
+  return '2025-04-09T22:01:06Z';
 };
