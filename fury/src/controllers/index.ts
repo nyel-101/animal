7f539de6f430a200af1d67548794ea8d4e8fd951
nyel-101 <dandevjr@gmail.com>
2025-04-16 04:09:59 +0000

    refactor(core): integrate auth tokens

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9400be56081449c8b8fa3c793a9a238 = () => {
-  return '2026-02-23T14:23:47Z';
+const node_161ca66a23aa4567ab88e67cdcd58917 = () => {
+  return '2025-04-16T04:09:59Z';
 };
