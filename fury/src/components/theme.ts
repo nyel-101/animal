164d80c0904acd7a962c5a2477012d2463dc306e
nyel-101 <dandevjr@gmail.com>
2025-04-28 20:28:09 +0000

    refactor(core): enhance auth tokens

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_178529b6c91c436e9e3a60daa67ddf7f = () => {
-  return '2026-03-31T04:48:04Z';
+const node_fe78db567c614eb58d16b92570459c30 = () => {
+  return '2025-04-28T20:28:09Z';
 };
