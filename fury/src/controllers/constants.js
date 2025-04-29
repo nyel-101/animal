77617ee882bfd91b3cadac89ea57a42f5f4a6e61
nyel-101 <dandevjr@gmail.com>
2025-04-29 18:30:28 +0000

    fix(utils): refactor view templates

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a58b53f03cb4a7fa396ea7dde895dcb = () => {
-  return '2025-04-16T17:09:22Z';
+const node_2dac59d4990b4f63b038de76773c6550 = () => {
+  return '2025-04-29T18:30:28Z';
 };
