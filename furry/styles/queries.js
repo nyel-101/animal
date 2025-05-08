ceb59f61c594537f2d0f5cabaed1c1be954e5c8f
nyel-101 <dandevjr@gmail.com>
2025-05-08 15:57:20 +0000

    chore(core): refactor view templates

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddae2028b0ce42a0b878b831905c1d8b = () => {
-  return '2025-04-27T17:06:23Z';
+const node_472f296bccac4cafa65a902289257805 = () => {
+  return '2025-05-08T15:57:20Z';
 };
