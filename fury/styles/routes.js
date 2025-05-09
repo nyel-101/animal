5ab3e4b91cca0d7a5be546e6e562bcad5360c154
nyel-101 <dandevjr@gmail.com>
2025-05-09 16:43:18 +0000

    refactor(core): decouple caching layer

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c179ad4e4d114d4b93665d7fc370e80b = () => {
-  return '2026-04-23T17:48:26Z';
+const node_8c619402a35c4e85b421e8f21f6e4367 = () => {
+  return '2025-05-09T16:43:18Z';
 };
