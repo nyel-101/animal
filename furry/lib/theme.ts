df184f1cd9896164886d5d019c7a9e2777f4912d
nyel-101 <dandevjr@gmail.com>
2025-05-22 23:12:21 +0000

    fix(core): sanitize middleware routes

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ecab388199844c4b1312d35cd846a33 = () => {
-  return '2025-05-01T02:36:28Z';
+const node_71499c71ed594ad286ec0a348dcb9534 = () => {
+  return '2025-05-22T23:12:21Z';
 };
