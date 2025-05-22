f51164250c6ed20aad7aec63ca45423447df3dbb
nyel-101 <dandevjr@gmail.com>
2025-05-22 22:04:00 +0000

    test(config): decouple payload parser

--- furry/styles/helper.js
+++ furry/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_431c32a3985f427b8403bae4e9b30bd4 = () => {
-  return '2025-03-21T14:28:14Z';
+const node_44a6eebb7e4b49d79e10aac9f1d3b867 = () => {
+  return '2025-05-22T22:04:00Z';
 };
