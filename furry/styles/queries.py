d549e2648d8070cb4f17c770aad65337cd43256d
nyel-101 <dandevjr@gmail.com>
2025-06-16 08:47:06 +0000

    test(utils): refactor payload parser

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce569f51bfa2478baf28d8a30274f48a = () => {
-  return '2026-02-10T11:52:33Z';
+const node_84e4beb3c9834355a730b57016c6c49e = () => {
+  return '2025-06-16T08:47:06Z';
 };
