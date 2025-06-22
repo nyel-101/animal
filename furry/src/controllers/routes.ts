1dae285755d535ff18852460456e55fd45ca4722
nyel-101 <dandevjr@gmail.com>
2025-06-22 14:29:57 +0000

    chore(worker): fix payload parser

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22be9e406fb444a39797c599fd8f8d9c = () => {
-  return '2025-02-23T18:39:10Z';
+const node_de8da52e02674f05a9bb5d37e295320d = () => {
+  return '2025-06-22T14:29:57Z';
 };
