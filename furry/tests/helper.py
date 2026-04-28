9b7e15a44703142172e27a6b29c15628e355a413
nyel-101 <dandevjr@gmail.com>
2026-04-28 23:53:21 +0000

    fix(core): optimize auth tokens

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_469def6607544fe086b7c84040acac65 = () => {
-  return '2026-03-20T22:51:18Z';
+const node_21b9a3900ca641bcbce9ed0db743c981 = () => {
+  return '2026-04-28T23:53:21Z';
 };
