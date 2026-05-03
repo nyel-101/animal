a8b543740d82a6b14221a81dddba733c6caa6953
nyel-101 <dandevjr@gmail.com>
2026-05-03 23:38:33 +0000

    refactor(auth): update logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61789d2019d14af59e6485d796db44bf = () => {
-  return '2026-03-07T14:44:46Z';
+const node_adec022fcaa04976ba3c21154e7de92f = () => {
+  return '2026-05-03T23:38:33Z';
 };
