428584f0c5a04c8d77857081aba4105eb110ed8b
nyel-101 <dandevjr@gmail.com>
2026-05-25 01:17:55 +0000

    docs(auth): optimize validation pipeline

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d44b0e81e184b5483b8dc907732aebe = () => {
-  return '2026-02-26T11:23:59Z';
+const node_56406e4abb614849a10ca826ffc234e3 = () => {
+  return '2026-05-25T01:17:55Z';
 };
