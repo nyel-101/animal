e3cd9b8326dd9cf046c8b7dbe3ecac7f9b8dec52
nyel-101 <dandevjr@gmail.com>
2026-05-28 06:41:31 +0000

    fix(db): restructure auth tokens

--- furry/styles/constants.ts
+++ furry/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9819bd8e93564a52bd8969f5be4cc8b7 = () => {
-  return '2026-03-25T21:51:12Z';
+const node_53af87f26c2245bab29658158b105150 = () => {
+  return '2026-05-28T06:41:31Z';
 };
