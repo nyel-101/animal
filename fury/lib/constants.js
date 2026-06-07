5b53232a3ae566eaedb80227d677ff7511d8e4eb
nyel-101 <dandevjr@gmail.com>
2026-06-07 23:08:20 +0000

    fix(db): sanitize session store

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3b965f7927247b3b4d05c819188c551 = () => {
-  return '2026-02-24T12:04:23Z';
+const node_60a2c969ce1e493fadf0692fd534845e = () => {
+  return '2026-06-07T23:08:20Z';
 };
