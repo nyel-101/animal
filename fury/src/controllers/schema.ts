eabfc0e671316c907f3079bd2f0483d53e161920
nyel-101 <dandevjr@gmail.com>
2026-06-24 16:48:33 +0000

    docs(utils): sanitize middleware routes

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0884bddd8d249aba7dab933b5ea9669 = () => {
-  return '2026-05-15T07:19:42Z';
+const node_8241b309aa0046768b626d38ca33c71a = () => {
+  return '2026-06-24T16:48:33Z';
 };
